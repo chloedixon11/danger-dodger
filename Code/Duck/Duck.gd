6f161be245dd07ce7e618151978b7754c6b77818
chloedixon11 <chloe.dixon@pcc.edu>
2026-08-31 13:18:56 -0700

    Duck tscn fixed, input added

--- Code/Duck/Duck.gd
+++ Code/Duck/Duck.gd
@@ -1,12 +1,19 @@
-extends Node2D
+extends Node3D
 class_name Duck
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("move_left"):
+		position.x -= 1
+		
+	if Input.is_action_just_pressed("move_right"):
+		position.x += 1
+		
+	if Input.is_action_just_pressed("move_up"):
+		position.z -= 1
+	
+	if Input.is_action_just_pressed("move_down"):
+		position.z += 1
+		
 	pass
