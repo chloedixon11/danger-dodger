70be0072c4e4dea2791228dcbb9cf5fb1d4e0822
chloedixon11 <chloe.dixon@pcc.edu>
2026-08-31 19:39:28 -0700

    Animal lerp, Reoriented lanes and vehicles along z-axis

--- Code/Duck/Duck.gd
+++ Code/Duck/Duck.gd
@@ -1,20 +1,50 @@
 extends Node3D
 class_name Duck
 
-var leap_distance = 1.0
+@onready var graphics: Node3D = $Graphics
+
+var leap_distance: float = 1.0
+var weight: float = 1.0
+var weight_speed: float = 3.0
+
+var current_spot: Vector3 = Vector3.ZERO
+var next_spot: Vector3 = Vector3.ZERO
+
+func _ready() -> void:
+	current_spot = position
+	next_spot = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("move_left"):
-		position.x -= leap_distance
+		#position.x -= leap_distance
+		next_spot = current_spot + Vector3.LEFT
+		weight = 0
+		graphics.rotation_degrees.y = 90
 		
 	if Input.is_action_just_pressed("move_right"):
-		position.x += leap_distance
+		#position.x += leap_distance
+		next_spot = current_spot + Vector3.RIGHT
+		weight = 0
+		graphics.rotation_degrees.y = -90
 		
 	if Input.is_action_just_pressed("move_up"):
-		position.z -= leap_distance
+		#position.z -= leap_distance
+		next_spot = current_spot + Vector3.FORWARD
+		weight = 0
+		graphics.rotation_degrees.y = 0
 	
 	if Input.is_action_just_pressed("move_down"):
-		position.z += leap_distance
+		#position.z += leap_distance
+		next_spot = current_spot + Vector3.BACK
+		weight = 0
+		graphics.rotation_degrees.y = 180
 		
-	pass
+	if weight < 1.0:
+		weight += weight_speed * delta
+	else:
+		weight = 1.0
+		current_spot = next_spot
+		
+	
+	position = lerp(current_spot, next_spot, weight)
--- Code/Vehicle/Vehicle.gd
+++ Code/Vehicle/Vehicle.gd
@@ -2,10 +2,10 @@ extends Node3D
 class_name Vehicle
 
 var speed: float = 3.0
-var kill_distance: float = 25
+var kill_distance: float = 10.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += speed * delta
-	if position.x > kill_distance:
-		position.x = -kill_distance
+	position.z += speed * delta
+	if position.z > kill_distance:
+		position.z = -kill_distance
